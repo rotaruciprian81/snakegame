1cb008b43752af8c313e665eb280e36249510949
Ciprian Rotaru <rotaruciprian81@yahoo.com>
2016-02-03 17:50:43 +0000

    Java SnakeGame
    In order to develop the Snake game, 6 classes have been created inside
    the package.

---
+++ src/snakegame/Direction.java
@@ -0,0 +1,35 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package snakegame;
+
+/**
+ *
+ * @author Ciprian Rotaru
+ */
+public enum Direction {
+
+	/**
+	 * Moving North (Up).
+	 */
+	North,
+	
+	/**
+	 * Moving East (Right).
+	 */
+	East,
+	
+	/**
+	 * Moving South (Down).
+	 */
+	South,
+	
+	/**
+	 * Moving West (Left).
+	 */
+	West
+	
+}
+
